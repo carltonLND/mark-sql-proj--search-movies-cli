8bf06bde4b2f6d0b7325079eaa4a29c080cd22f9
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-08-10 11:39:38 +0100

    end client

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,6 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
+const client = new Client({ database: "omdb" });
 console.log("Welcome to search-movies-cli!");
+client.end();
